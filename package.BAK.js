726510733908c5f531ec741de14d673f15ee4962
jandreotti <joa.andreotti@gmail.com>
2023-07-28 13:40:32 -0300

    fix7

---
+++ package.BAK.js
@@ -0,0 +1,35 @@
+// {
+// 	"name": "wsp-example",
+// 	"version": "1.0.0",
+// 	"description": "",
+// 	"main": "/dist/app.js",
+// 	"scripts": {
+// 		"start": "tsc && node dist/app.js",
+// 		"lint": "eslint . --ext .ts",
+// 		"test": "echo \"Error: no test specified\" && exit 1"
+// 	},
+// 	"keywords": [],
+// 	"author": "jandreotti",
+// 	"license": "ISC",
+// 	"devDependencies": {
+// 		"@types/md5": "^2.3.2",
+// 		"@types/os-utils": "0.0.1",
+// 		"@types/qr-image": "^3.2.5",
+// 		"@typescript-eslint/eslint-plugin": "^5.42.0",
+// 		"@typescript-eslint/parser": "^5.42.0",
+// 		"eslint": "^8.26.0",
+// 		"typescript": "^4.8.4"
+// 	},
+// 	"dependencies": {
+// 		"axios": "^1.1.3",
+// 		"express": "^4.18.2",
+// 		"md5": "^2.3.0",
+// 		"moment": "^2.29.4",
+// 		"os-utils": "0.0.14",
+// 		"qr-image": "^3.2.0",
+// 		"whatsapp-web.js": "^1.21.0",
+// 		"puppeteer":"^20.9.0",
+// 		// "puppeteer2": "npm:puppeteer@20.5.0",
+// 		// "puppeteer": "npm:puppeteer@^20.9.0"
+// 	}
+// }
