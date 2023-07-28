9ddc4ec1bc36b300517b6fa622a70254139fe7f4
jandreotti <joa.andreotti@gmail.com>
2023-07-28 15:04:46 -0300

    fix 12

--- src/workers/wCheckCitaLMDLahabana.ts
+++ src/workers/wCheckCitaLMDLahabana.ts
@@ -49,11 +49,11 @@ const run = async () => {
 
 		const browser = await puppeteer.launch({
 			args: [
-				// '--no-sandbox',
-				// '--disable-setuid-sandbox',
-				// '--disable-background-timer-throttling',
-				// '--disable-backgrounding-occluded-windows',
-				// '--disable-renderer-backgrounding',
+				'--no-sandbox',
+				'--disable-setuid-sandbox',
+				'--disable-background-timer-throttling',
+				'--disable-backgrounding-occluded-windows',
+				'--disable-renderer-backgrounding',
 			],
 			headless: 'new', // trabaja en background ->  con este anda bien el waitforNetworkIdle
 			//  headless: false, //  VIEJO -> para ver que hace el explorador en la pagina
