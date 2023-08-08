e4e002a7b785de25764f727ff03a6186adc89fda
jandreotti <joa.andreotti@gmail.com>
2023-08-07 23:52:09 -0300

    Actualizar wCheckCitaLMDLahabana.ts

--- src/workers/wCheckCitaLMDLahabana.ts
+++ src/workers/wCheckCitaLMDLahabana.ts
@@ -224,16 +224,21 @@ const run = async () => {
 		console.error('aa');
 		await wait(1000);
 
-		await page.goto(url, { waitUntil: 'load',
-   timeout: 60 * 1000
-  });
+		//await page.goto(url, { waitUntil: 'load',
+   //timeout: 60 * 1000
+  //});
+await page.goto(url);
 
 		// Esto de aqui lo pongo para que este activa la pagina y funcione lo de abajo (AL FINAL SE SOLUCIONO CON headless: 'new',)
 		//FUENTE: https://github.com/puppeteer/puppeteer/issues/3339
 		//const session = await page.target().createCDPSession();
 		//await session.send('Page.enable');
 		//await session.send('Page.setWebLifecycleState', { state: 'active' });
+console.error("aaa");
 
+await page.waitForSelector('a[href='https://www.citaconsular.es/es/hosteds/widgetdefault/28330379fc95acafd31ee9e8938c278ff']', {
+			 visible: true,
+		});
 		console.error(3);
 		// Hacer click en el boton
 		const a = await page.$(
