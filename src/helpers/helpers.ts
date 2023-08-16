a8158aa4d59a7e11cb56ce822dda0442037f76ea
jandreotti <joa.andreotti@gmail.com>
2023-08-16 09:30:07 -0300

    fix

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 export const log = (mensaje: string, archivo: string = "log.txt") => {
 
-	fs.appendFile(archivo, mensaje, function (err) {
+	fs.appendFile(archivo, mensaje + "\n", function (err) {
 		if (err) throw err;
 		// console.log('Saved!');
 	});
